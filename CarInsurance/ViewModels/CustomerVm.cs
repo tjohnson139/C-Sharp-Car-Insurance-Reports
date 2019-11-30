a160c93a000dd69d341eaa7f5460a6551f2eef2a
Tyler Johnson <tylerjohnson139@gmail.com>
2019-11-30 03:15:29 -0800

    Initial Commit

---
+++ CarInsurance/ViewModels/CustomerVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsurance.ViewModels
+{
+    public class CustomerVm
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string DOB { get; set; }
+        public int CarYear { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public string DUI { get; set; }
+        public int Tickets { get; set; }
+        public string Coverage { get; set; }
+        public int Quote { get; set; }
+    }
+}
---
+++ Insurance/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Insurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Insurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Customer(string firstName, string lastName, string emailAddress,
+                                    string DOB, int CarYear, string CarMake, string CarModel,
+                                    string DUI, int Tickets, string Coverage, int Quote)
+        {
+            using (db_insuranceEntities1 db = new db_insuranceEntities1())
+            {
+                var customer = new Customer();
+                customer.FirstName = firstName;
+                customer.LastName = lastName;
+                customer.EmailAddress = emailAddress;
+                customer.DOB = Convert.ToDateTime(DOB).Date;
+                customer.CarYear = Convert.ToInt32(CarYear);
+                customer.CarMake = CarMake;
+                customer.CarModel = CarModel;
+                customer.DUI = DUI;
+                customer.Tickets = Convert.ToInt32(Tickets);
+                customer.Coverage = Coverage;
+                customer.Quote = Convert.ToInt32(Quote);
+
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return View("Quote");
+            }
+        }
+    }
+}
